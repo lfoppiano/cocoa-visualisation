53194fa63f26d69e94ed05ea26cf2ef60ed8bab9
Luca Foppiano <Foppiano.Luca@nims.go.jp>
2023-09-21 12:10:46 +0900

    update fetch script

--- concepts_visualisation/openalex/fetch.py
+++ concepts_visualisation/openalex/fetch.py
@@ -1,5 +1,8 @@
+import argparse
 import json
 import os
+import sys
+from pathlib import Path
 
 import pyalex
 from dotenv import load_dotenv
@@ -11,68 +14,97 @@ load_dotenv()
 pyalex.config.email = os.environ['OPENALEX_CONFIG_EMAIL']
 pyalex.config.api_key = os.environ['OPENALEX_API_KEY']
 
-pager = Works().filter(concept={"id": "https://openalex.org/C555008776"}).paginate(per_page=200, n_max=200000)
 
-id = 0
-for page in tqdm(pager):
-    work_page = []
-    with open("resources/data/openalex/data_dump" + str(id) + ".json", 'w') as fw:
-        for work in page:
-            output_work = {}
+def fetch_openalex_works(concept_id, output_path, per_page=200, n_max=2000000):
+    pager = Works().filter(concept={"id": "https://openalex.org/" + concept_id}).paginate(per_page=per_page,
+                                                                                          n_max=n_max)
+    id = 0
+    for page in tqdm(pager):
+        work_page = []
+        with open(os.path.join(output_path, "data_dump" + str(id) + ".json", 'w')) as fw:
+            for work in page:
+                output_work = get_work(work)
+                work_page.append(output_work)
 
-            for key in ['id', 'doi', 'title', 'display_name', 'publication_year', 'publication_date', 'language']:
-                if key in work:
-                    output_work[key] = work[key]
+            json.dump(work_page, fw)
+            id += 1
 
-            authorships = work['authorships'] if 'authorships' in work else []
-            output_work['authors'] = []
-            for author in authorships:
-                output_author = {}
 
-                if 'id' in author['author']:
-                    output_author["id"] = author['author']['id']
-                if 'position' in author['author']:
-                    output_author["position"] = author['author_position']
-                if 'orcid' in author['author']:
-                    output_author["orcid"] = author['author']['orcid']
-                if 'display_name' in author['author']:
-                    output_author["display_name"] = author['author']['display_name']
+def get_work(input_work):
+    output_work = {}
+    for key in ['id', 'doi', 'title', 'display_name', 'publication_year', 'publication_date', 'language']:
+        if key in input_work:
+            output_work[key] = input_work[key]
+    authorships = input_work['authorships'] if 'authorships' in input_work else []
+    output_work['authors'] = []
+    for author in authorships:
+        output_author = {}
 
-                affiliations = author['institutions'] if 'institutions' in author else []
-                output_affiliations = []
-                for affiliation in affiliations:
-                    output_affiliation = {}
-                    if 'id' in affiliation:
-                        output_affiliation["id"] = affiliation['id']
-                    if "country_code" in affiliation:
-                        output_affiliation['country'] = affiliation['country_code']
-                    if 'type' in affiliation:
-                        output_affiliation["type"] = affiliation['type']
-                    if 'display_name' in affiliation:
-                        output_affiliation['display_name'] = affiliation['display_name']
+        if 'id' in author['author']:
+            output_author["id"] = author['author']['id']
+        if 'position' in author['author']:
+            output_author["position"] = author['author_position']
+        if 'orcid' in author['author']:
+            output_author["orcid"] = author['author']['orcid']
+        if 'display_name' in author['author']:
+            output_author["display_name"] = author['author']['display_name']
 
-                    output_affiliations.append(output_affiliation)
-                output_author['institutions'] = output_affiliations
-                output_work['authors'].append(output_author)
+        affiliations = author['institutions'] if 'institutions' in author else []
+        output_affiliations = []
+        for affiliation in affiliations:
+            output_affiliation = {}
+            if 'id' in affiliation:
+                output_affiliation["id"] = affiliation['id']
+            if "country_code" in affiliation:
+                output_affiliation['country'] = affiliation['country_code']
+            if 'type' in affiliation:
+                output_affiliation["type"] = affiliation['type']
+            if 'display_name' in affiliation:
+                output_affiliation['display_name'] = affiliation['display_name']
 
-            if work['abstract_inverted_index'] is not None:
-                output_work['abstract'] = work['abstract']
+            output_affiliations.append(output_affiliation)
+        output_author['institutions'] = output_affiliations
+        output_work['authors'].append(output_author)
+    if input_work['abstract_inverted_index'] is not None:
+        output_work['abstract'] = input_work['abstract']
+    if 'concepts' in input_work:
+        work_concepts = []
+        for concept in input_work['concepts']:
+            work_concepts.append(
+                {
+                    "id": concept['id'],
+                    "wikidata": concept['wikidata'],
+                    "display_name": concept['display_name'],
+                    "level": concept['level'],
+                    "score": concept['score']
+                }
+            )
 
-            if 'concepts' in work:
-                work_concepts = []
-                for concept in work['concepts']:
-                    work_concepts.append(
-                        {
-                            "id": concept['id'],
-                            "wikidata": concept['wikidata'],
-                            "display_name": concept['display_name'],
-                            "level": concept['level']
-                        }
-                    )
+        output_work['concepts'] = work_concepts
+    return output_work
 
-                output_work['concepts'] = work_concepts
 
-            work_page.append(output_work)
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(
+        description="Fetch OpenAlex works")
 
-        json.dump(work_page, fw)
-        id += 1
+    parser.add_argument("--base-concept",
+                        required=True,
+                        default="C555008776")
+    parser.add_argument("--output",
+                        help="Output directory. If it does not exists it will be created.",
+                        required=True,
+                        type=Path)
+
+    args = parser.parse_args()
+
+    base_concept = args.base_concept
+    output = args.output
+
+    if not os.path.isdir(output):
+        print("--output should indicate a directory. ")
+        sys.exit(-1)
+
+    os.makedirs(output)
+
+    fetch_openalex_works(base_concept, output)
