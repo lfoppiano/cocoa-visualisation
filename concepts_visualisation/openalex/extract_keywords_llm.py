26b1f6c906c2c4af1c4ad72c04d663f7ed6d2a4c
Luca Foppiano <Foppiano.Luca@nims.go.jp>
2023-10-12 08:32:13 +0900

    update llm extractor

--- concepts_visualisation/openalex/extract_keywords_llm.py
+++ concepts_visualisation/openalex/extract_keywords_llm.py
@@ -1,4 +1,7 @@
 import dotenv
+from langchain.chat_models import PromptLayerChatOpenAI
+from langchain.prompts import ChatPromptTemplate, HumanMessagePromptTemplate, SystemMessagePromptTemplate, \
+    PromptTemplate
 from langchain.schema import OutputParserException
 
 dotenv.load_dotenv(override=True)
@@ -9,11 +12,88 @@ from typing import List
 from langchain.output_parsers import PydanticOutputParser
 from pydantic import BaseModel
 
-from commons.openai import chat4_prompt_layer, chat_prompt_layer
-from grobid_magneto.data_preparation.process_openai_extraction_quantities import extract_entities, _parse_json
 
+PROMPT_TEMPLATE_CHAT_SYSTEM = """Use the following pieces of context to answer the users question. 
+If you don't know the answer, just say that you don't know, don't try to make up an answer.
+----------------\n{text}"""
 PROMPT_TEMPLATE_USER = """Extract a list of 10 keywords sorted by importance."""
 
+chat_prompt_layer = PromptLayerChatOpenAI(model_name="gpt-3.5-turbo",
+                                          frequency_penalty=0.1,
+                                          temperature=0,
+                                          return_pl_id=True,
+                                          pl_tags=["chatgpt"],
+                                          )
+
+chat4_prompt_layer = PromptLayerChatOpenAI(model_name="gpt-4",
+                                           frequency_penalty=0.1,
+                                           temperature=0,
+                                           return_pl_id=True,
+                                           pl_tags=["gpt4"],
+                                           )
+
+def get_prompt(user_template, format_instructions=None, hints=None) -> ChatPromptTemplate:
+    system_message_prompt = SystemMessagePromptTemplate.from_template(PROMPT_TEMPLATE_CHAT_SYSTEM)
+    human_message_prompt = HumanMessagePromptTemplate.from_template(user_template)
+    if hints:
+        user_template += "\nHere some examples appearing in the text: " + str(hints)
+
+    if format_instructions:
+        user_template += "\n{format_instructions}"
+
+        human_message_prompt = HumanMessagePromptTemplate(
+            prompt=PromptTemplate(
+                template=user_template,
+                input_variables=[],
+                partial_variables={"format_instructions": format_instructions})
+        )
+
+    prompt = ChatPromptTemplate.from_messages([system_message_prompt, human_message_prompt])
+
+    return prompt
+
+
+def extract_entities(text, prompt_template, llm, output_parser_class=None, hints=None):
+    format_instructions = None
+    if output_parser_class:
+        output_parser = PydanticOutputParser(pydantic_object=output_parser_class)
+        format_instructions = output_parser.get_format_instructions()
+
+    prompt_chat_template = get_prompt(prompt_template, format_instructions, hints=hints)
+    prompt_text = prompt_chat_template.format_messages(text=text)
+    # print("Nb Tokens", chat.get_num_tokens_from_messages(prompt_text))
+
+    results = llm(prompt_text)
+
+    if output_parser_class:
+        output = output_parser.parse(results.content)
+        output = output_parser_class.parse_to_list(output)
+    else:
+        output = results.content
+
+    return output
+
+
+def _parse_json(response, llm, output_parser):
+    system_message = "You are an useful assistant expert in materials science, physics, and chemistry " \
+                     "that can process text and transform it to JSON."
+    human_message = """Transform the text between three double quotes in JSON.\n\n\n\n
+        {format_instructions}\n\nText: \"\"\"{text}\"\"\""""
+
+    system_message_prompt = SystemMessagePromptTemplate.from_template(system_message)
+    human_message_prompt = HumanMessagePromptTemplate.from_template(human_message)
+
+    prompt_template = ChatPromptTemplate.from_messages([system_message_prompt, human_message_prompt])
+
+    results = llm(
+        prompt_template.format_prompt(
+            text=response,
+            format_instructions=output_parser.get_format_instructions()
+        ).to_messages()
+    )
+    parsed_output = output_parser.parse(results.content)
+
+    return parsed_output
 
 class ListOfKeywordsOutputParser(BaseModel):
     keywords: List[str]
@@ -25,8 +105,7 @@ class ListOfKeywordsOutputParser(BaseModel):
 
 def extract_keywords(text, llm):
     try:
-        keywords = extract_entities(text, PROMPT_TEMPLATE_USER, llm,
-                                                  ListOfKeywordsOutputParser)
+        keywords = extract_entities(text, PROMPT_TEMPLATE_USER, llm, ListOfKeywordsOutputParser)
     except OutputParserException as ope:
         output_data_quantities_raw = extract_entities(text, PROMPT_TEMPLATE_USER, llm)
         if output_data_quantities_raw.startswith("I don't know"):
